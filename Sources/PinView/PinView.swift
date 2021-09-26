5d56e1b205fad09d5563fefcba92fad991828708
Dennis Hernandez <dennis264@mac.com>
2021-09-25 19:27:41 -0700

    Visuals mostly complete.

--- Sources/PinView/PinView.swift
+++ Sources/PinView/PinView.swift
@@ -8,36 +8,101 @@
 import SwiftUI
 import ManualStack
 
+//public typealias Pins = [Pin]
+
 
 public struct PinViewState: Codable {
     var background:Color?
     var horizontal:Bool = true
 }
 
+public enum PinType: Int, CaseIterable {
+    case rPi, ic, pwm
+}
+
 struct PinView: View {
+    var delegate: PinViewDelegate? = nil
     var state: PinViewState
-    var pins: [Pin] = []
+    var backgroundColor: Color  {
+        get {
+            switch self.type {
+            case .rPi:
+                return .clear
+            case .ic:
+                return .black.opacity(0.75)
+            case .pwm:
+                return .clear
+                //            default:
+                //              return .clear
+            }
+        }
+    }
+    
+    var pins: [Pin] {
+        get {
+            var internalPins: [Pin] = []
+            
+            switch self.type {
+            case .rPi:
+                internalPins = setPinType(type: .rPi)
+            case .ic:
+                internalPins = setPinType(type: .ic)
+            case .pwm:
+                internalPins = setPinType(type: .pwm)
+            }
+            
+            return internalPins
+        }
+    }
+    
+    func setPinType(type:PinType) -> [Pin]{
+        var thePins: [Pin] = []
+        for (_,pin) in pins.enumerated() {
+            var newPin = pin
+            newPin.type = type.rawValue
+            thePins.append(newPin)
+        }
+        return thePins
+    }
+    
+    var type: PinType = .rPi
     
     var body: some View {
-        ManualStack(stackType: .Vertical){
+        let isHorizontal = (type == .ic)
+        ManualStack(isVertical: !isHorizontal) {
             ForEach(pins, id:\.self){ pin in
                 let pinLocation = pins.firstIndex(of: pin) ?? 0
                 
-                if pinLocation % 2 == 0 {
-                    ManualStack(stackType: .Horizontal){PinControl(pin: pin)
-                    PinControl(pin: pins[pinLocation + 1])
+                if (pinLocation % 2 == 0 && type != .pwm) {
+                    ManualStack(isVertical: isHorizontal) {PinControl(pin: pin)
+                        PinControl(pin: pins[pinLocation + 1])
                     }
+                } else if (type == .pwm) {
+                    PinControl(pin: pin)
                 } else {
                     EmptyView()
                 }
-            }
+            }.padding(Edge.Set.all, 9.0)
+        }.background(self.backgroundColor)
+        
+    }
+
+}
+
+struct PinView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            PinView(state: PinViewState(background: .clear, horizontal: false))
+                .preferredColorScheme(.light)
+            PinView(state: PinViewState(background: .clear, horizontal: false))
+                .preferredColorScheme(.dark)
         }
     }
 }
 
-
-struct PinView_Previews: PreviewProvider {
-    static var previews: some View {
-        PinView(state: PinViewState(background: .clear, horizontal: false), pins: rPiPins())
-    }
+protocol PinViewDelegate {
+    func pinAction(pin:Pin)
+}
+protocol PinDelegate {
+    func pinAction(pin:Pin)
 }
