26d6f86f6a5d72b45e915644cadfb1aee461de9a
Dennis Hernandez <dennis264@mac.com>
2021-09-25 23:45:29 -0700

    Styling

--- Sources/PinView/PinControl.swift
+++ Sources/PinView/PinControl.swift
@@ -15,24 +15,24 @@ struct PinStateView:View {
         ZStack {
             if pin.type == PinType.pwm.rawValue {
                 RoundedRectangle(cornerRadius: 9.0)
-//                    .frame(width:pin.squareHeight(), height: pin.squareHeight())
+                    .frame(width:pin.squareHeight(), height: pin.squareHeight())
                     .foregroundColor(pin.color)
                     .clipped()
             } else {
                 Circle()
                     .foregroundColor(Color.gray)
-//                    .frame(width:pin.squareHeight() - 1, height: pin.squareHeight() - 1)
+                    .frame(width:pin.squareHeight() - 1, height: pin.squareHeight() - 1)
                     .shadow(color: Color.black, radius: 1.0, x: 1.0, y: 1.0)
                 Circle()
                     .strokeBorder((Color.primary), lineWidth: 3.0)
-//                    .frame(width:pin.squareHeight(), height: pin.squareHeight())
+                    .frame(width:pin.squareHeight(), height: pin.squareHeight())
                     .foregroundColor(Color.clear)
                     .clipped()
             }
             Text(pin.state.text)
                 .fontWeight(.medium)
-//                .frame(width: pin.squareHeight(), height: pin.squareHeight())
-                .font(.system(.title, design: .monospaced))
+                .frame(width: pin.squareHeight(), height: pin.squareHeight())
+                .font(.system(.subheadline, design: .monospaced))
                 .foregroundColor(Color.primary)
                 .shadow(color: Color.primary, radius: 1.0, x: 0.0, y: 0.0)
                 
@@ -54,14 +54,14 @@ struct PinLabel: View {
             if pin.type != PinType.pwm.rawValue {
                 RoundedRectangle(cornerRadius: 9.0)
                     .clipped()
-//                    .frame(width: pin.frame().width, height: pin.frame().height)
+                    .frame(width: pin.frame().width, height: pin.frame().height)
                     .foregroundColor(pin.color)
                     .shadow(color: Color.black, radius: 1.0, x: 1.0, y: 1.0)
                     .saturation(0.75)
             } else {
                 RoundedRectangle(cornerRadius: 9.0)
                     .clipped()
-//                    .frame(width: pin.frame().width, height: pin.frame().height)
+                    .frame(width: pin.frame().width, height: pin.frame().height)
                     .foregroundColor(Color.gray)
 
                     .shadow(color: Color.black, radius: 1.0, x: 1.0, y: 1.0)
@@ -69,11 +69,11 @@ struct PinLabel: View {
                 
             }
             Text(pin.label())
-//                .frame(width: pin.frame().width, height: pin.frame().height)
+                .frame(width: pin.frame().width, height: pin.frame().height)
                 .allowsTightening(true)
                 .flipsForRightToLeftLayoutDirection(true)
                 .clipped()
-                .font(.system(.title, design: .monospaced))
+                .font(.system(.subheadline, design: .monospaced))
                 .foregroundColor(Color.primary)
                 .shadow(color: Color.primary, radius: 1.0, x: 0.0, y: 0.0)
         }
@@ -94,11 +94,11 @@ public struct PinControl: View {
                     if (pin.type == PinType.pwm.rawValue) {
                         RoundedRectangle(cornerRadius: 9.0)
                             .clipped()
-//                            .frame(width:pin.squareHeight(), height: pin.squareHeight())
+                            .frame(width:pin.squareHeight(), height: pin.squareHeight())
                             .foregroundColor(.black)
                         RoundedRectangle(cornerRadius: 9.0)
                             .clipped()
-//                            .frame(width:pin.squareHeight(), height: pin.squareHeight())
+                            .frame(width:pin.squareHeight(), height: pin.squareHeight())
                             .foregroundColor(.red)
                     }
                     pinControlBlock()
